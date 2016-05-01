ab5446b3b4891d75f93209c7a69c724eb42f114b
Chris Stoafer <cstoafer@gmail.com>
2016-05-01 15:49:38 -0400

    increase coverage utils.py

--- boardroom/tests/test_utils.py
+++ boardroom/tests/test_utils.py
@@ -1,8 +1,40 @@
 import os
 import unittest
+import cPickle as pickle
 
 from boardroom import utils
 from boardroom.tests.utils import TEST_DIRECTORY
+
+class TestCacheDict(unittest.TestCase):
+    def setUp(self):
+        self.tmpsavefile = os.path.join(TEST_DIRECTORY, 'savetest.p')
+        if os.path.exists(self.tmpsavefile):
+            os.remove(self.tmpsavefile)
+        self.tmploadfile = os.path.join(TEST_DIRECTORY, 'loadtest.p')
+        if os.path.exists(self.tmploadfile):
+            os.remove(self.tmploadfile)
+        self.testdict = {'a': 1, 'b': 2, 'c': 3}
+        with open(self.tmploadfile, 'w') as f:
+            pickle.dump(self.testdict, f)
+
+    def tearDown(self):
+        if os.path.exists(self.tmpsavefile):
+            os.remove(self.tmpsavefile)
+        if os.path.exists(self.tmploadfile):
+            os.remove(self.tmploadfile)
+
+    def test_load_cache_dict(self):
+        loaddict = utils.load_cache_dict('loadtest.p', TEST_DIRECTORY)
+        self.assertEqual(loaddict, self.testdict)
+
+    def test_save_cache_dict(self):
+        utils.save_cache_dict(self.testdict, 'savetest.p', TEST_DIRECTORY)
+        self.assertTrue(os.path.exists(self.tmpsavefile))
+
+    def test_load_no_file(self):
+        loaddict = utils.load_cache_dict('not_a_file', TEST_DIRECTORY)
+        self.assertEqual(loaddict, dict())
+
 
 class TestSilentremove(unittest.TestCase):
     def setUp(self):
