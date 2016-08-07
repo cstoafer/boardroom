e0648df14a25aad5adbd6fb5ddeb95913c765c13
Chris Stoafer <cstoafer@gmail.com>
2016-08-07 14:32:08 -0400

    add setup and requirements

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+def find_requirements():
+    with open('requirements.txt') as f:
+        required = [x.strip() for x in f.read().splitlines()]
+        return [x for x in required if x.strip() and not x.strip().startswith("#")]
+
+setup(
+    name='boardroom',
+    version='0.0.0',
+    packages=['boardroom', 'boardroom.test'],
+    description='Observe and analyze insider trading from SEC filings.',
+    url='https://github.com/cstoafer/boardroom',
+    install_requires=find_requirements()
+)
